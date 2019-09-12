7b1624855e0bfd38b97fa907eb524335b68724e5
Evan Richard <evan.richard@algorand.com>
2019-09-12 12:33:41 -0400

    start branch / add test

--- tests/5.Transaction.js
+++ tests/5.Transaction.js
@@ -155,6 +155,31 @@ describe('Sign', function () {
             assert.deepStrictEqual(reencRep, encRep);
         });
 
+        it('should correctly serialize and deserialize an asset creation transaction from msgpack representation', function() {
+            let o = {
+                "from": "XMHLMNAVJIMAW2RHJXLXKKK4G3J3U6VONNO3BTAQYVDC3MHTGDP3J5OCRU",
+                "fee": 10,
+                "firstRound": 51,
+                "lastRound": 61,
+                "note": new Uint8Array([123, 12, 200]),
+                "genesisHash": "JgsgCaCTqIaLeVhyL6XlRu3n7Rfk2FxMeK+wRSaQ7dI=",
+                "voteKey": "5/D4TQaBHfnzHI2HixFV9GcdUaGFwgCQhmf0SVhwaKE=",
+                "selectionKey": "oImqaSLjuZj63/bNSAjd+eAh5JROOJ6j1cY4eGaJGX4=",
+                "voteFirst": 123,
+                "voteLast": 456,
+                "voteKeyDilution": 1234,
+                "genesisID": "",
+                "type": "acfg"
+            };
+            let expectedTxn = new transaction.Transaction(o);
+            let encRep = expectedTxn.get_obj_for_encoding();
+            const encTxn = encoding.encode(encRep);
+            const decEncRep = encoding.decode(encTxn);
+            let decTxn = transaction.Transaction.from_obj_for_encoding(decEncRep);
+            const reencRep = decTxn.get_obj_for_encoding();
+            assert.deepStrictEqual(false, true); // always fail until this test is written
+        });
+
         it('reserializes correctly no genesis ID', function() {
             let o = {
                 "from": "XMHLMNAVJIMAW2RHJXLXKKK4G3J3U6VONNO3BTAQYVDC3MHTGDP3J5OCRU",
