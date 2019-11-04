fe39eb06b36de4db4075da6e2b5c79ae8a6f641f
Francisco Gamundi <52399794+fxgamundi@users.noreply.github.com>
2019-11-04 15:38:47 +0100

    Add optional headers parameter to assetInformation (#77)
    Review: https://github.com/algorand/js-algorand-sdk/pull/77

--- src/client/algod.js
+++ src/client/algod.js
@@ -197,10 +197,11 @@ function Algod(token = '', baseServer = "http://r2.algorand.network", port = 418
     /**
      * assetInformation returns the information for the asset with the passed creator and index
      * @param index number
+     * @param headers, optional
      * @returns {Promise<*>}
      */
-    this.assetInformation = async function (index) {
-        let res = await c.get("/v1/asset/" + index);
+    this.assetInformation = async function (index, headers={}) {
+        let res = await c.get("/v1/asset/" + index, {}, headers);
         return res.body;
     };
 
