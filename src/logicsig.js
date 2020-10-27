91685efa64cc3fb8237439258d6e367d41829f17
Evan Richard <evan.richard@algorand.com>
2020-10-27 18:44:52 -0400

    prefer buffer.isbuffer

--- src/logicsig.js
+++ src/logicsig.js
@@ -20,7 +20,7 @@ class LogicSig {
             assert(Array.isArray(args))
             function checkType(arg) {
                 let theType = typeof arg;
-                return ((theType == "string") || (theType == "number") || (arg.constructor == Uint8Array) || (arg.constructor == Buffer));
+                return ((theType == "string") || (theType == "number") || (arg.constructor == Uint8Array) || (Buffer.isBuffer(arg)));
             }
             assert(args.every(checkType))
         }
