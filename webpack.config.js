84748747be0304b7b104d12311140d464a8403bf
Jacob Daitzman <jdtzmn@gmail.com>
2021-03-11 14:26:27 -0500

    Incorporate TypeScript compilation into build steps

--- webpack.config.js
+++ webpack.config.js
@@ -12,4 +12,17 @@ module.exports = {
     },
   },
   devtool: 'source-map',
+  resolve: {
+    // Add '.ts' as resolvable extensions
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      // All files with a '.ts' extension will be handled by 'ts-loader'.
+      { test: /\.ts$/, loader: 'ts-loader' },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: 'source-map-loader' },
+    ],
+  },
 };
