1e71932192cc759ddb8865383d90dab733aa2762
Jason Paulos <jasonpaulos@users.noreply.github.com>
2021-03-17 16:55:05 -0400

    Fix genesis path

--- src/client/v2/algod/genesis.js
+++ src/client/v2/algod/genesis.js
@@ -3,7 +3,7 @@ const { JSONRequest } = require('../jsonrequest');
 class Genesis extends JSONRequest {
   // eslint-disable-next-line no-underscore-dangle,class-methods-use-this
   _path() {
-    return '/v2/genesis';
+    return '/genesis';
   }
 }
 
