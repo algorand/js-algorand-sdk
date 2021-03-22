2aebe9fa25b2c48add5914ca315a30cc1a5c39c2
Jacob Daitzman <jdtzmn@gmail.com>
2021-03-22 09:58:39 -0400

    Migrate `algod.js` to TypeScript

--- src/client/v2/algod/algod.ts
+++ src/client/v2/algod/algod.ts
@@ -1,24 +1,29 @@
-const client = require('../../client');
-const ai = require('./accountInformation');
-const blk = require('./block');
-const compile = require('./compile');
-const dryrun = require('./dryrun');
-const gasbid = require('./getAssetByID');
-const gapbid = require('./getApplicationByID');
-const hc = require('./healthCheck');
-const pti = require('./pendingTransactionInformation');
-const pt = require('./pendingTransactions');
-const ptba = require('./pendingTransactionsByAddress');
-const srt = require('./sendRawTransaction');
-const status = require('./status');
-const sab = require('./statusAfterBlock');
-const sp = require('./suggestedParams');
-const supply = require('./supply');
-const versions = require('./versions');
-const genesis = require('./genesis');
-const proof = require('./proof');
+import { HTTPClient } from '../../client';
+import { IntDecoding } from '../../../types/intDecoding';
+import modelsv2 from './models/types';
+import AccountInformation from './accountInformation';
+import Block from './block';
+import Compile from './compile';
+import Dryrun from './dryrun';
+import GetAssetByID from './getAssetByID';
+import GetApplicationByID from './getApplicationByID';
+import HealthCheck from './healthCheck';
+import PendingTransactionInformation from './pendingTransactionInformation';
+import PendingTransactions from './pendingTransactions';
+import PendingTransactionsByAddress from './pendingTransactionsByAddress';
+import SendRawTransaction from './sendRawTransaction';
+import Status from './status';
+import StatusAfterBlock from './statusAfterBlock';
+import SuggestedParams from './suggestedParams';
+import Supply from './supply';
+import Versions from './versions';
+import Genesis from './genesis';
+import Proof from './proof';
 
-class AlgodClient {
+export default class AlgodClient {
+  private c: HTTPClient;
+  private intDecoding: IntDecoding;
+
   constructor(
     token = '',
     baseServer = 'http://r2.algorand.network',
@@ -26,14 +31,13 @@ class AlgodClient {
     headers = {}
   ) {
     // workaround to allow backwards compatibility for multiple headers
-    let tokenHeader = token;
+    let tokenHeader: string | Record<string, string> = token;
     if (typeof tokenHeader === 'string') {
       tokenHeader = { 'X-Algo-API-Token': tokenHeader };
     }
 
     // Get client
-    this.c = new client.HTTPClient(tokenHeader, baseServer, port, headers);
-
+    this.c = new HTTPClient(tokenHeader, baseServer, port, headers);
     this.intDecoding = 'default';
   }
 
@@ -43,7 +47,7 @@ class AlgodClient {
    *   response for request. Must be one of "default", "safe", "mixed", or "bigint". See
    *   JSONRequest.setIntDecoding for more details about what each method does.
    */
-  setIntEncoding(method) {
+  setIntEncoding(method: IntDecoding) {
     this.intDecoding = method;
   }
 
@@ -55,91 +59,91 @@ class AlgodClient {
   }
 
   healthCheck() {
-    return new hc.HealthCheck(this.c);
+    return new HealthCheck(this.c);
   }
 
   versionsCheck() {
-    return new versions.Versions(this.c);
+    return new Versions(this.c);
   }
 
-  sendRawTransaction(stxOrStxs) {
-    return new srt.SendRawTransaction(this.c, stxOrStxs);
+  sendRawTransaction(stxOrStxs: Uint8Array | Uint8Array[]) {
+    return new SendRawTransaction(this.c, stxOrStxs);
   }
 
   /**
    * Returns the given account's information.
    * @param {string} account The address of the account to look up.
    */
-  accountInformation(account) {
-    return new ai.AccountInformation(this.c, this.intDecoding, account);
+  accountInformation(account: string) {
+    return new AccountInformation(this.c, this.intDecoding, account);
   }
 
   /**
    * Gets the block info for the given round.
    * @param {number} roundNumber The round number of the block to get.
    */
-  block(roundNumber) {
-    return new blk.Block(this.c, roundNumber);
+  block(roundNumber: number) {
+    return new Block(this.c, roundNumber);
   }
 
   /**
    * Returns the transaction information for a specific pending transaction.
    * @param {string} txid The TxID string of the pending transaction to look up.
    */
-  pendingTransactionInformation(txid) {
-    return new pti.PendingTransactionInformation(this.c, txid);
+  pendingTransactionInformation(txid: string) {
+    return new PendingTransactionInformation(this.c, txid);
   }
 
   /**
    * Returns transactions that are pending in the pool.
    */
   pendingTransactionsInformation() {
-    return new pt.PendingTransactions(this.c);
+    return new PendingTransactions(this.c);
   }
 
   /**
    * Returns transactions that are pending in the pool sent by a specific sender.
    * @param {string} address The address of the sender.
    */
-  pendingTransactionByAddress(address) {
-    return new ptba.PendingTransactionsByAddress(this.c, address);
+  pendingTransactionByAddress(address: string) {
+    return new PendingTransactionsByAddress(this.c, address);
   }
 
   /**
    * Retrieves the StatusResponse from the running node.
    */
   status() {
-    return new status.Status(this.c, this.intDecoding);
+    return new Status(this.c, this.intDecoding);
   }
 
   /**
    * Waits for a specific round to occur then returns the StatusResponse for that round.
    * @param {number} round The number of the round to wait for.
    */
-  statusAfterBlock(round) {
-    return new sab.StatusAfterBlock(this.c, this.intDecoding, round);
+  statusAfterBlock(round: number) {
+    return new StatusAfterBlock(this.c, this.intDecoding, round);
   }
 
   /**
    * Returns the common needed parameters for a new transaction.
    */
   getTransactionParams() {
-    return new sp.SuggestedParams(this.c);
+    return new SuggestedParams(this.c);
   }
 
   /**
    * Gets the supply details for the specified node's ledger.
    */
   supply() {
-    return new supply.Supply(this.c, this.intDecoding);
+    return new Supply(this.c, this.intDecoding);
   }
 
-  compile(source) {
-    return new compile.Compile(this.c, source);
+  compile(source: string) {
+    return new Compile(this.c, source);
   }
 
-  dryrun(dr) {
-    return new dryrun.Dryrun(this.c, dr);
+  dryrun(dr: modelsv2.DryrunSource) {
+    return new Dryrun(this.c, dr);
   }
 
   /**
@@ -147,8 +151,8 @@ class AlgodClient {
    * special addresses.
    * @param {number} index The asset ID to look up.
    */
-  getAssetByID(index) {
-    return new gasbid.GetAssetByID(this.c, this.intDecoding, index);
+  getAssetByID(index: number) {
+    return new GetAssetByID(this.c, this.intDecoding, index);
   }
 
   /**
@@ -156,15 +160,15 @@ class AlgodClient {
    * and clear programs, global and local schemas, and global state.
    * @param {number} index The application ID to look up.
    */
-  getApplicationByID(index) {
-    return new gapbid.GetApplicationByID(this.c, this.intDecoding, index);
+  getApplicationByID(index: number) {
+    return new GetApplicationByID(this.c, this.intDecoding, index);
   }
 
   /**
    * Returns the entire genesis file.
    */
   genesis() {
-    return new genesis.Genesis(this.c, this.intDecoding);
+    return new Genesis(this.c, this.intDecoding);
   }
 
   /**
@@ -172,9 +176,7 @@ class AlgodClient {
    * @param {number} round The round in which the transaction appears.
    * @param {string} txID The transaction ID for which to generate a proof.
    */
-  getProof(round, txID) {
-    return new proof.Proof(this.c, this.intDecoding, round, txID);
+  getProof(round: number, txID: string) {
+    return new Proof(this.c, this.intDecoding, round, txID);
   }
 }
-
-module.exports = { AlgodClient };
