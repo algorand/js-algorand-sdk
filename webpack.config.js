49bd2bc8a4936102fc2b1a3958f12cb485832646
Jacob Daitzman <jdtzmn@gmail.com>
2021-03-30 16:38:56 -0400

    Improve build steps for TypeScript migration (#323)

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './index.ts',
   output: {
     filename: 'algosdk.min.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/browser'),
     library: {
       type: 'window',
       name: 'algosdk',
@@ -25,10 +25,18 @@ module.exports = {
   module: {
     rules: [
       // All files with a '.ts' extension will be handled by 'ts-loader'.
-      { test: /\.ts$/, loader: 'ts-loader' },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          configFile: path.resolve(__dirname, 'tsconfig-browser.json'),
+        },
+      },
 
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { test: /\.js$/, loader: 'source-map-loader' },
     ],
+    // Don't parse tweetnacl module — https://github.com/dchest/tweetnacl-js/wiki/Using-with-Webpack
+    noParse: [/[\\/]tweetnacl[\\/]/, /[\\/]tweetnacl-auth[\\/]/],
   },
 };
