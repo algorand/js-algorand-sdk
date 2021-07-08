a281f60f41a88473ce33bce833ccbc40c523d2c3
Jason Paulos <jasonpaulos@users.noreply.github.com>
2021-07-08 08:31:03 -0700

    Address feedback

--- src/client/v2/algod/models/base.ts
+++ src/client/v2/algod/models/base.ts
@@ -23,7 +23,6 @@ function _get_obj_for_encoding(
   binary: boolean
 ): Record<string, any>;
 function _get_obj_for_encoding(val: any, binary: boolean): any {
-  /* eslint-disable no-unused-vars */
   let targetPropValue: any;
 
   if (val instanceof Uint8Array) {
@@ -48,19 +47,19 @@ function _get_obj_for_encoding(val: any, binary: boolean): any {
   }
   return targetPropValue;
 }
-/* eslint-disable no-redeclare */
+/* eslint-enable no-redeclare,no-unused-vars */
 
 export default class BaseModel {
   attribute_map: Record<string, string>;
 
   /**
    * Get an object ready for encoding to either JSON or msgpack.
-   * @param binary Use true to indicate that the encoding can handle raw binary objects
+   * @param binary - Use true to indicate that the encoding can handle raw binary objects
    *   (Uint8Arrays). Use false to indicate that raw binary objects should be converted to base64
    *   strings. True should be used for objects that will be encoded with msgpack, and false should
    *   be used for objects that will be encoded with JSON.
    */
-  get_obj_for_encoding(binary: boolean = false) {
+  get_obj_for_encoding(binary = false) {
     const obj: Record<string, any> = {};
 
     for (const prop of Object.keys(this.attribute_map)) {
