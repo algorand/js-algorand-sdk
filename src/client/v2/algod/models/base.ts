f369d88271c3fe7de4e06083d162f807b7987b04
Jason Paulos <jasonpaulos@users.noreply.github.com>
2021-07-07 17:00:38 -0700

    Add docstring

--- src/client/v2/algod/models/base.ts
+++ src/client/v2/algod/models/base.ts
@@ -53,6 +53,13 @@ function _get_obj_for_encoding(val: any, binary: boolean): any {
 export default class BaseModel {
   attribute_map: Record<string, string>;
 
+  /**
+   * Get an object ready for encoding to either JSON or msgpack.
+   * @param binary Use true to indicate that the encoding can handle raw binary objects
+   *   (Uint8Arrays). Use false to indicate that raw binary objects should be converted to base64
+   *   strings. True should be used for objects that will be encoded with msgpack, and false should
+   *   be used for objects that will be encoded with JSON.
+   */
   get_obj_for_encoding(binary: boolean = false) {
     const obj: Record<string, any> = {};
 
