42f5078e935a91ce0883197fed3f38df2368ec62
Jason Paulos <jasonpaulos@users.noreply.github.com>
2021-09-28 11:36:11 -0700

    Test more ops

--- tests/8.LogicSig.ts
+++ tests/8.LogicSig.ts
@@ -1092,6 +1092,13 @@ describe('Program validation', () => {
       );
       // byte "testdata"; sha512_256; byte 0x79bfa8245aeac0e714b7bd2b3252d03979e5e7a43cb039715a5f8109a7dd9ba1; byte 0x0753d317e54350d1d102289afbde3002add4529f10b9f7d3d223843985de62e0; byte 0x03abfb5e6e331fb871e423f354e2bd78a384ef7cb07ac8bbf27d2dd1eca00e73c1; ecdsa_pk_decompress Secp256k1; ecdsa_verify Secp256k1
       assert.ok(logic.checkProgram(program));
+
+      // cover, uncover, log
+      program = new Uint8Array(
+        Buffer.from('058001618001628001634e024f025050b08101', 'hex')
+      );
+      // byte "a"; byte "b"; byte "c"; cover 2; uncover 2; concat; concat; log; int 1
+      assert.ok(logic.checkProgram(program));
     });
   });
 });
