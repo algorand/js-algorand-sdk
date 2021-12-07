9dd99e96ede0bb945605425ab0a194c84ee85086
Jason Paulos <jasonpaulos@users.noreply.github.com>
2021-12-07 13:03:09 -0800

    Fix wait for confirmation function (#480)
    * Fix wait for confirmation function
    
    * Respond to feedback

--- src/wait.ts
+++ src/wait.ts
@@ -26,21 +26,33 @@ export async function waitForConfirmation(
 
   /* eslint-disable no-await-in-loop */
   while (currentRound < startRound + waitRounds) {
-    const pendingInfo = await client.pendingTransactionInformation(txid).do();
+    let poolError = false;
+    try {
+      const pendingInfo = await client.pendingTransactionInformation(txid).do();
 
-    if (pendingInfo['confirmed-round']) {
-      // Got the completed Transaction
-      return pendingInfo;
-    }
+      if (pendingInfo['confirmed-round']) {
+        // Got the completed Transaction
+        return pendingInfo;
+      }
 
-    if (pendingInfo['pool-error']) {
-      // If there was a pool error, then the transaction has been rejected!
-      throw new Error(`Transaction Rejected: ${pendingInfo['pool-error']}`);
+      if (pendingInfo['pool-error']) {
+        // If there was a pool error, then the transaction has been rejected
+        poolError = true;
+        throw new Error(`Transaction Rejected: ${pendingInfo['pool-error']}`);
+      }
+    } catch (err) {
+      // Ignore errors from PendingTransactionInformation, since it may return 404 if the algod
+      // instance is behind a load balancer and the request goes to a different algod than the
+      // one we submitted the transaction to
+      if (poolError) {
+        // Rethrow error only if it's because the transaction was rejected
+        throw err;
+      }
     }
 
     await client.statusAfterBlock(currentRound).do();
     currentRound += 1;
   }
   /* eslint-enable no-await-in-loop */
-  throw new Error(`Transaction not confirmed after ${waitRounds} rounds!`);
+  throw new Error(`Transaction not confirmed after ${waitRounds} rounds`);
 }
