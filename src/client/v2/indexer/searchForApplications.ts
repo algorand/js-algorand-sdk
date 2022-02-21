5a383ed877f31ac384f3cb6397e7fa9101c31764
Fionna <fionnacst@gmail.com>
2022-02-21 17:26:35 +0800

    Add docs for searchForApplications

--- src/client/v2/indexer/searchForApplications.ts
+++ src/client/v2/indexer/searchForApplications.ts
@@ -1,30 +1,105 @@
 import JSONRequest from '../jsonrequest';
 
+/**
+ * Returns information about indexed applications.
+ *
+ * #### Example
+ * ```typescript
+ * const apps = await indexerClient.searchForApplications().do();
+ * ```
+ *
+ * [Response data schema details](https://developer.algorand.org/docs/rest-apis/indexer/#get-v2applications)
+ * @category GET
+ */
 export default class SearchForApplications extends JSONRequest {
+  /**
+   * @returns `/v2/applications`
+   */
   // eslint-disable-next-line class-methods-use-this
   path() {
     return '/v2/applications';
   }
 
-  // application ID for filter, as int
+  /**
+   * Application ID for filter, as int
+   *
+   * #### Example
+   * ```typescript
+   * const appId = 60553466;
+   * const apps = await indexerClient
+   *        .searchForApplications()
+   *        .index(appId)
+   *        .do();
+   * ```
+   * @remarks Alternatively, use `indexerClient.lookupApplications(appId).do()`
+   * @param index
+   */
   index(index: number) {
     this.query['application-id'] = index;
     return this;
   }
 
-  // token for pagination
+  /**
+   * Specify the next page of results.
+   *
+   * #### Example
+   * ```typescript
+   * const maxResults = 20;
+   * const nextToken = "APA6C7C3NCANRPIBUWQOF7WSKLJMK6RPQUVFLLDV4U5WCQE4DEF26D4E3E";
+   * const apps = await indexerClient
+   *        .searchForApplications()
+   *        .limit(maxResults)
+   *        .next(nextToken)
+   *        .do();
+   * ```
+   * @param nextToken - provided by the previous results.
+   */
   nextToken(next: string) {
     this.query.next = next;
     return this;
   }
 
-  // limit results for pagination
+  /**
+   * Limit results for pagination.
+   *
+   * #### Example
+   * ```typescript
+   * const maxResults = 20;
+   * const apps = await indexerClient
+   *        .searchForApplications()
+   *        .limit(maxResults)
+   *        .do();
+   * ```
+   *
+   * @param limit - maximum number of results to return.
+   */
   limit(limit: number) {
     this.query.limit = limit;
     return this;
   }
 
-  // include all items including closed accounts, deleted applications, destroyed assets, opted-out asset holdings, and closed-out application localstates
+  /**
+   * Includes all items including closed accounts, deleted applications, destroyed assets, opted-out asset holdings, and closed-out application localstates
+   *
+   * #### Example 1
+   * ```typescript
+   * const apps = await indexerClient
+   *        .searchForApplications()
+   *        .includeAll(false)
+   *        .do();
+   * ```
+   *
+   * #### Example 2
+   * ```typescript
+   * const apps = await indexerClient
+   *        .searchForApplications()
+   *        .includeAll()
+   *        .do();
+   * ```
+   *
+   * @param value - default true when called without passing a value
+   * @category query
+   */
   includeAll(value = true) {
     this.query['include-all'] = value;
     return this;
