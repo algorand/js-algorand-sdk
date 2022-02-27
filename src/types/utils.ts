2092598181d20896181026e9fdfff3d884e3448f
Joe Polny <joepolny@gmail.com>
2022-02-27 16:54:25 +0000

    add Expand to expand types for IntelliSense

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,4 +1,14 @@
 /**
+ * Expands types for IntelliSense so they are more human readable
+ * See https://stackoverflow.com/a/69288824
+ */
+export type Expand<T> = T extends (...args: infer A) => infer R
+  ? (...args: Expand<A>) => Expand<R>
+  : T extends infer O
+  ? { [K in keyof O]: O[K] }
+  : never;
+
+/**
  * Same as TypeScript's Pick, but will distribute the Pick over unions
  */
 export type DistributivePick<T, K extends keyof T> = T extends unknown
