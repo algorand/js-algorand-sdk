7b15d3ab49232f7da9741695f29ec68702e230db
algochoi <86622919+algochoi@users.noreply.github.com>
2022-06-09 20:31:12 -0400

    Fix box ref catch

--- src/boxStorage.ts
+++ src/boxStorage.ts
@@ -12,9 +12,12 @@ function translateBoxReference(
   try {
     index = foreignApps.indexOf(referenceId) + 1;
   } catch (err) {
-    throw new Error(`Box ref with appId ${referenceId} not in foreign-apps`);
+    // Foreign app array cannot be empty unless the reference ID is itself.
+    if (referenceId !== 0 && referenceId !== appIndex) {
+      throw new Error(`Box ref with appId ${referenceId} not in foreign-apps`);
+    }
   }
-  // Check if the app referenced is itself after checking the foreign apps array
+  // Check if the app referenced is itself after checking the foreign apps array.
   if (index === 0 && referenceId !== 0 && referenceId !== appIndex) {
     throw new Error(`Box ref with appId ${referenceId} not in foreign-apps`);
   }
