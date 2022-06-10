0c3c4e1a6daec714a65e2f62b3670f3151410bb2
algochoi <86622919+algochoi@users.noreply.github.com>
2022-06-09 20:07:33 -0400

    Fix err case

--- src/boxStorage.ts
+++ src/boxStorage.ts
@@ -7,17 +7,18 @@ function translateBoxReference(
 ) {
   const referenceId = reference.appIndex;
   const referenceName = reference.name;
+  let index = 0;
   // Foreign apps start from index 1; index 0 is its own app ID.
   try {
-    const index = foreignApps.indexOf(referenceId) + 1;
-    // Check if the app referenced is itself after checking the foreign apps array
-    if (referenceId !== 0 && referenceId !== appIndex) {
-      throw new Error(`Box ref with appId ${referenceId} not in foreign-apps`);
-    }
-    return { i: index, n: referenceName };
-  } catch (TypeError) {
+    index = foreignApps.indexOf(referenceId) + 1;
+  } catch (err) {
     throw new Error(`Box ref with appId ${referenceId} not in foreign-apps`);
   }
+  // Check if the app referenced is itself after checking the foreign apps array
+  if (index === 0 && referenceId !== 0 && referenceId !== appIndex) {
+    throw new Error(`Box ref with appId ${referenceId} not in foreign-apps`);
+  }
+  return { i: index, n: referenceName };
 }
 
 /**
