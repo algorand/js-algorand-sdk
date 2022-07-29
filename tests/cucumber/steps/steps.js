3a8cbb162a242a5fbb34c7ad6309fcfed858a58c
michaeldiamant <michaeldiamant@users.noreply.github.com>
2022-07-29 14:58:44 -0400

    Revert unrelated change

--- tests/cucumber/steps/steps.js
+++ tests/cucumber/steps/steps.js
@@ -881,13 +881,11 @@ module.exports = function getSteps(options) {
   });
 
   Then('the transaction should go through', async function () {
-    let info = await this.acl.pendingTransactionInformation(this.txid);
-    assert.deepStrictEqual(true, 'type' in info);
-    // let localParams = await this.acl.getTransactionParams();
-    // this.lastRound = localParams.lastRound;
     waitForAlgodInDevMode();
-    info = await this.acl.transactionById(this.txid);
+    const info = await this.acl.pendingTransactionInformation(this.txid);
     assert.deepStrictEqual(true, 'type' in info);
+    // info = await this.acl.transactionById(this.txid);
+    // assert.deepStrictEqual(true, 'type' in info);
   });
 
   Then('I can get the transaction by ID', async function () {
