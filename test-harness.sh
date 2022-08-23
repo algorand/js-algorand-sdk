f698e96778944ace85e2c7315d1432f448abb652
Zeph Grunschlag <zeph@algorand.com>
2022-08-23 12:25:29 -0500

    Bugfix: Pass verbosity to the harness and sandbox

--- test-harness.sh
+++ test-harness.sh
@@ -15,6 +15,8 @@ set +a
 rootdir=$(dirname "$0")
 pushd "$rootdir"
 
+echo "$THIS: VERBOSE_HARNESS=$VERBOSE_HARNESS"
+
 ## Reset test harness
 if [ -d "$SDK_TESTING_HARNESS" ]; then
   pushd "$SDK_TESTING_HARNESS"
@@ -28,11 +30,13 @@ fi
 git clone --depth 1 --single-branch --branch "$SDK_TESTING_BRANCH" "$SDK_TESTING_URL" "$SDK_TESTING_HARNESS"
 
 
+echo "$THIS: OVERWRITE_TESTING_ENVIRONMENT=$OVERWRITE_TESTING_ENVIRONMENT"
 if [[ $OVERWRITE_TESTING_ENVIRONMENT == 1 ]]; then
   echo "$THIS: OVERWRITE downloaded $SDK_TESTING_HARNESS/.env with $ENV_FILE:"
   cp "$ENV_FILE" "$SDK_TESTING_HARNESS"/.env
 fi
 
+echo "$THIS: REMOVE_LOCAL_FEATURES=$REMOVE_LOCAL_FEATURES"
 ## Copy feature files into the project resources
 if [[ $REMOVE_LOCAL_FEATURES == 1 ]]; then
   echo "$THIS: OVERWRITE wipes clean $TEST_DIR/features"
@@ -50,7 +54,11 @@ echo "$THIS: seconds it took to get to end of cloning and copying: $(($(date "+%
 
 ## Start test harness environment
 pushd "$SDK_TESTING_HARNESS"
-./scripts/up.sh
+
+[[ "$VERBOSE_HARNESS" = 1 ]] && V_FLAG="-v" || V_FLAG=""
+echo "$THIS: standing up harnness with command [./up.sh $V_FLAG]"
+./scripts/up.sh "$V_FLAG"
+
 popd
 echo "$THIS: seconds it took to finish testing sdk's up.sh: $(($(date "+%s") - START))s"
 echo ""
