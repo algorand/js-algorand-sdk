06680d5828f231812ed8b17d4abf489f1f0b3005
Jason Paulos <jasonpaulos@users.noreply.github.com>
2022-10-27 10:11:22 -0700

    Update docstring

--- src/client/urlTokenBaseHTTPClient.ts
+++ src/client/urlTokenBaseHTTPClient.ts
@@ -73,6 +73,9 @@ export class URLTokenBaseHTTPClient implements BaseHTTPClient {
   /**
    * Compute the URL for a path relative to the instance's address
    * @param relativePath - A path string
+   * @param query - An optional key-value object of query parameters to add to the URL. If the
+   *   relativePath already has query parameters on it, the additional parameters defined here will
+   *   be added to the URL without modifying those (unless a key collision occurs).
    * @returns A URL string
    */
   private getURL(relativePath: string, query?: Query<string>): string {
