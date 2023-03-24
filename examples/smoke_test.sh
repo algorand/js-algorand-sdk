467db4d62039f56a2610e097aea0d6a35a41cd7f
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-03-24 09:12:56 -0400

    Try using the version that should be in .bin

--- examples/smoke_test.sh
+++ examples/smoke_test.sh
@@ -7,26 +7,26 @@ export KMD_PORT="60001"
 npm install ts-node
 
 echo "Running accounts.ts"
-ts-node accounts.ts
+node_modules/.bin/ts-node accounts.ts
 echo "Running app.ts"
-ts-node app.ts
+node_modules/.bin/ts-node app.ts
 echo "Running asa.ts"
-ts-node asa.ts
+node_modules/.bin/ts-node asa.ts
 echo "Running atc.ts"
-ts-node atc.ts
+node_modules/.bin/ts-node atc.ts
 echo "Running atomics.ts"
-ts-node atomics.ts
+node_modules/.bin/ts-node atomics.ts
 echo "Running codec.ts"
-ts-node codec.ts
+node_modules/.bin/ts-node codec.ts
 echo "Running debug.ts"
-ts-node debug.ts
+node_modules/.bin/ts-node debug.ts
 echo "Running indexer.ts"
-ts-node indexer.ts
+node_modules/.bin/ts-node indexer.ts
 echo "Running kmd.ts"
-ts-node kmd.ts
+node_modules/.bin/ts-node kmd.ts
 echo "Running lsig.ts"
-ts-node lsig.ts
+node_modules/.bin/ts-node lsig.ts
 echo "Running overview.ts"
-ts-node overview.ts
+node_modules/.bin/ts-node overview.ts
 echo "Running participation.ts"
-ts-node participation.ts
+node_modules/.bin/ts-node participation.ts
