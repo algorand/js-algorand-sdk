2a849d1fb769c0953eeb216640a00fe26519bde1
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-03-24 10:20:48 -0400

    replace shebang in smoketest

--- examples/smoke_test.sh
+++ examples/smoke_test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 export ALGOD_PORT="60000"
 export INDEXER_PORT="59999"
@@ -15,28 +15,3 @@ for file in *; do
         fi
     fi
 done
-
-#echo "Running accounts.ts"
-#../node_modules/.bin/ts-node accounts.ts
-#echo "Running app.ts"
-#../node_modules/.bin/ts-node app.ts
-#echo "Running asa.ts"
-#../node_modules/.bin/ts-node asa.ts
-#echo "Running atc.ts"
-#../node_modules/.bin/ts-node atc.ts
-#echo "Running atomics.ts"
-#../node_modules/.bin/ts-node atomics.ts
-#echo "Running codec.ts"
-#../node_modules/.bin/ts-node codec.ts
-#echo "Running debug.ts"
-#../node_modules/.bin/ts-node debug.ts
-#echo "Running indexer.ts"
-#../node_modules/.bin/ts-node indexer.ts
-#echo "Running kmd.ts"
-#../node_modules/.bin/ts-node kmd.ts
-#echo "Running lsig.ts"
-#../node_modules/.bin/ts-node lsig.ts
-#echo "Running overview.ts"
-#../node_modules/.bin/ts-node overview.ts
-#echo "Running participation.ts"
-#../node_modules/.bin/ts-node participation.ts
