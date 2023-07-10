c161ec2ca130a31d0686647036334cc1586404f7
algochoi <86622919+algochoi@users.noreply.github.com>
2023-07-10 16:32:17 -0400

    Revert smoke test

--- examples/smoke_test.sh
+++ examples/smoke_test.sh
@@ -9,7 +9,7 @@ for file in *; do
     # Check if the file ends with ".ts"
     if [[ $file == *.ts ]]; then
         # Check if the filename is not "utils.ts"
-        if [[ $file != "utils.ts" || $file != "kmd.ts" ]]; then
+        if [[ $file != "utils.ts" ]]; then
             # Call the file using `ts-node`
             ../node_modules/.bin/ts-node "$file"
             # Check if the test failed
