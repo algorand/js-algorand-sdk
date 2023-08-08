68cdf070c64c8ce4ee942fe3c3904cf8b0a8072a
algochoi <86622919+algochoi@users.noreply.github.com>
2023-08-08 16:12:20 -0400

    Add address bytes length check in encodeAddress

--- src/encoding/address.ts
+++ src/encoding/address.ts
@@ -100,6 +100,12 @@ export function isValidAddress(address: string) {
  * @returns the address and checksum encoded as a string.
  */
 export function encodeAddress(address: Uint8Array) {
+  // Check address length
+  if (
+    address.length !==
+    ALGORAND_ADDRESS_BYTE_LENGTH - ALGORAND_CHECKSUM_BYTE_LENGTH
+  )
+    throw new Error(MALFORMED_ADDRESS_ERROR_MSG);
   // compute checksum
   const checksum = nacl
     .genericHash(address)
