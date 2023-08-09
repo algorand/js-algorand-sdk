bb5a76b69ad1143d5bf6eba01f6b336d42a1d2d6
Hang Su <hang.su@algorand.com>
2023-08-09 09:57:07 -0400

    remove comment

--- tests/cucumber/steps/steps.js
+++ tests/cucumber/steps/steps.js
@@ -4821,7 +4821,6 @@ module.exports = function getSteps(options) {
       const optionList = execTraceOptions.split(',');
 
       assert.ok(this.simulateRequest);
-      // TODO: check if this is the best practice
       this.simulateRequest.execTraceConfig = new algosdk.modelsv2.SimulateTraceConfig(
         {
           enable: true,
