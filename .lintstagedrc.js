2e1d45574d080beb4cf96abf4951cdc08b9dd6eb
Robby Uitbeijerse <robbyuitbeijerse@gmail.com>
2023-12-01 20:31:23 +0100

    chore: migrate prettier & ESlint to Biome

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{js,ts,md,json,yml}': 'prettier --write',
-  '*.{js,ts}': 'eslint --cache --fix',
+  "*.{js,ts,md,json,yml}": "biome format --write",
+  "*.{js,ts}": "biome lint --apply-unsafe",
 };
